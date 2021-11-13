569bf89913da9d702c55c8433c61d4b0072a8078
athira0710 <94222741+athira0710@users.noreply.github.com>
2021-11-13 17:15:33 +0530

    Create task2.cpp

---
+++ task2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ float f0; 
+ 
+  float f1;
+  
+   cout<<"enter two numbers: ";
+   cin>>f0;
+   cin>>f1;
+   cout<<"product ="<<f0*f1;
+  
+    return 0;
