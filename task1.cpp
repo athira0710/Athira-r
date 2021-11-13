c3cd2f2694ccaee08240aa0e579186613739dd1c
athira0710 <94222741+athira0710@users.noreply.github.com>
2021-11-13 17:01:10 +0530

    Create task1.cpp

---
+++ task1.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int i; 
+  char c;
+  float f;
+  double d;
+   cout<<"size of char : "<<sizeof(c)<<"bytes ";
+   
+   cout<<"size of int : "<<sizeof(i)<<"bytes ";
+   
+      cout<<"size of float : "<<sizeof(f)<<"bytes ";
+
+
+   cout<<"size of double : "<<sizeof(d)<<"bytes ";
+    return 0;
+}
